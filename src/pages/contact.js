cbaa942a119c2858a011459f384def61f74a88ce
Kelly Peng <mingyu.peng@stonybrook.edu>
2022-02-05 01:29:03 -0500

    add calendly pluggin to the contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,7 @@
 import * as React from "react";
+import { InlineWidget } from "react-calendly";
+import { Typography, Grid, IconButton } from '@mui/material';
+import EmailIcon from '@mui/icons-material/Email';
 
 // components
 import Layout from "../components/layout";
@@ -7,6 +10,21 @@ import Layout from "../components/layout";
 const ContactPage = () => {
   return (
     <Layout pageTitle="Kelly Peng | Contact">
+      {/* Calendly inline widget */}
+      <div>
+        <InlineWidget 
+          url="https://calendly.com/mingyukellypeng/30min" 
+          styles={{ minWidth: '320px', height: '700px' }}
+          pageSettings={{ 
+            "backgroundColor": "FFFFFF",
+            "hideEventTypeDetails": false,
+            "hideLandingPageDetails": false,
+            "primaryColor": "FFBBD6",
+            "textColor": "4d5055",
+            "hideGdprBanner": true,
+          }} 
+        />
+      </div>
     </Layout>
   );
 };
